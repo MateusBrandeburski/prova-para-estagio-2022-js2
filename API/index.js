ce984ff57a4949af8f2e9351f57bf336e479f6e4
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2022-07-02 13:33:17 -0300

    Preparando para deploy no Heroko

--- API/index.js
+++ API/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express()
 
-app.listen('3000')
+const port = process.env.PORT || 3000
+
+app.listen(port)
 
 app.route('/dashcommercetest').get((req, res) => {
 
