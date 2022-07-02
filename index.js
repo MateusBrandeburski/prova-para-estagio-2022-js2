51ff552f249cc58891427dc6c9f3238f90dc1fc2
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2022-07-02 16:32:36 -0300

    retirando pasta API

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8877;
+
+app.route('/dashcommercetest').get((req, res) => {
+
+    const username = req.query.user
+    const password = req.query.password
+
+    if (username === 'dashcommerce' && password === 'chooseMe'){
+        return res.json({ mensagem: `My full name is Mateus Brandeburski Ramos. I want this job!` });
+
+    } else {
+        return res.json({ mensagem: `Wrong credentials.` });
+    }
+});
+
+app.listen(port, () => {
+    console.log('Server online!');
+});
