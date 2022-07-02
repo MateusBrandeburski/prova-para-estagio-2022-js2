4da337f204f149677f1866644098f8c297d2b3fa
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2022-07-01 21:49:30 -0300

    consegui fazer uma segunda rota

--- API/index.js
+++ API/index.js
@@ -3,3 +3,9 @@ const express = require('express')
 const app = express()
 
 app.listen('3000')
+
+
+
+app.route('/sobre').get( (req,res) => res.send("My full name is Mateus Brandeburski. I want this job!") )
+
+app.route('/').post((req, res) => console.log(req.body))
