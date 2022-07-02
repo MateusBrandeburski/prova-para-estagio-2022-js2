9993d582d49d277870e8e4f995b6de5439c7da75
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2022-07-02 14:59:59 -0300

    tentativa 4 de deploy

--- API/index.js
+++ API/index.js
@@ -1,10 +1,7 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
-
-const port = process.env.PORT || 3000
-
-app.listen(port)
+const port = process.env.PORT || 8877;
 
 app.route('/dashcommercetest').get((req, res) => {
 
@@ -12,9 +9,13 @@ app.route('/dashcommercetest').get((req, res) => {
     const password = req.query.password
 
     if (username === 'dashcommerce' && password === 'chooseMe'){
-        return res.json({ mensagem: `My full name is Mateus Brandeburski Ramos. I want this job!` })
+        return res.json({ mensagem: `My full name is Mateus Brandeburski Ramos. I want this job!` });
 
     } else {
-        return res.json({ mensagem: `Wrong credentials.` })
+        return res.json({ mensagem: `Wrong credentials.` });
     }
-})
+});
+
+app.listen(port, () => {
+    console.log('Servidor rodando!');
+});
